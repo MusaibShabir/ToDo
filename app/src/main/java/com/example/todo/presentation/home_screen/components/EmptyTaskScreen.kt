77a4dcd01af544df654fd31faeccfbaa479da8c9
Musaib Shabir <bnny633@gmail.com>
2024-02-09 17:35:23 +0530

    import optimisation

--- app/src/main/java/com/example/todo/presentation/home_screen/components/EmptyTaskScreen.kt
+++ app/src/main/java/com/example/todo/presentation/home_screen/components/EmptyTaskScreen.kt
@@ -1,11 +1,9 @@
 package com.example.todo.presentation.home_screen.components
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -13,9 +11,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
 import com.example.todo.R
 
 @Composable
